0eca9ed802564e400e0bf1e57db87ee00513731a
Bob Nguyen <drpebkac@foifoi.lol>
2020-10-07 23:17:14 +1100

    Linux VM

--- CreateAZVM.ps1
+++ CreateAZVM.ps1
@@ -1,6 +1,6 @@
-$WindowsAZVM = New-AzVMConfig -VMName CCVAZVMWI01 -VMSize Standard_B2S
-Set-AzVMOperatingSystem -VM $WindowsAZVM -Windows -ComputerName CCVAZVMWI01 -Credential $Creds -TimeZone "AUS Eastern Standard Time"
-Set-AzVMSourceImage -VM $WindowsAZVM -PublisherName MicrosoftWindowsDesktop -Offer "Windows-10" -Skus "19h2-pro" -Version "Latest"
-Set-AzVMOSDisk -VM $WindowsAZVM -Windows -DiskSizeInGB 127 -CreateOption FromImage -Caching ReadWrite
-Add-AzVMNetworkInterface -VM $WindowsAZVM -Id "/subscriptions/8681398b-3eff-4f81-9ec9-07e0c5f4745c/resourceGroups/CCV-GROUP-ITINFRA-TST01/providers/Microsoft.Network/virtualNetworks/Int0"
+$LinuxAZVM = New-AzVMConfig -VMName "CCVAZVMLI01" -VMSize "Standard_B1S"
+Set-AzVMOperatingSystem -VM $LinuxAZVM -Linux -ComputerName "CCVAZVMLI01" -Credential $Creds
+Set-AzVMSourceImage -VM $LinuxAZVM -PublisherName "Canonical" -Offer "UbuntuServer" -Skus "19.04" -Version "Latest"
+Set-AzVMOSDisk -VM $LinuxAZVM -Linux -DiskSizeInGB 30 -CreateOption FromImage -Caching ReadWrite
+Add-AzVMNetworkInterface -VM $LinuxAZVM -Id "/subscriptions/8681398b-3eff-4f81-9ec9-07e0c5f4745c/resourceGroups/GRP-AZ-TST/providers/Microsoft.Network/networkInterfaces/int0"
 
